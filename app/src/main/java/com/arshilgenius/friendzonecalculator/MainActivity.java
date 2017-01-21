252f6c415bdfe0c9103f8e7e7faf3d465ef318c4
Arshilgenius <Arshilgenius@gmail.com>
2017-01-21 20:23:29 +0530

    added the java methods lets see what happens

--- app/src/main/java/com/arshilgenius/friendzonecalculator/MainActivity.java
+++ app/src/main/java/com/arshilgenius/friendzonecalculator/MainActivity.java
@@ -2,12 +2,38 @@ package com.arshilgenius.friendzonecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int friendZone =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public int increment(View view){
+        friendZone=friendZone+1;
+        return friendZone;
+    }
+
+    public int decrement(View view){
+        friendZone=friendZone-1;
+        return friendZone;
+    }
+
+    public void onSubmit(int friendZone){
+        if(friendZone>3)
+            displayYup();
+        else displayNo();
+    }
+public void displayYup(){
+    TextView textview = (TextView) findViewById(R.id.result);
+            textview.setText("SHITTTTT !!!! Friendzoned Ho Gaye Aap LOL");
+}
+    public void displayNo(){
+        TextView textview = (TextView) findViewById(R.id.result);
+        textview.setText("Bach Gayee Aap Friendzone nhi hue ");
     }
 }
